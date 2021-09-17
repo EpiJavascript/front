1d3af7b0eb2c6d950ba4e844c3805f4d04e32e75
samyL <salamy-@hotmail.fr>
2021-09-17 09:21:21 +0200

    Routing and form
    Register page beginning, home page redirect

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { withRouter } from "react-router";
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        if (!props.user) {
+            this.props.history.push('/Register');
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Home</p>
+            </div>
+        );
+    }   
+}
+
+export default withRouter(Home);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
@@ -9,7 +11,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+        </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
